aca2c54ab807db87e09e366f4409adf2a311a219
Eeshwar S <86971180+darkboi3301@users.noreply.github.com>
2023-11-01 00:12:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ import nmap
 from io import StringIO
 
 
+
+# set page config
 st.set_page_config(
     page_title="WARP", page_icon="🚀", initial_sidebar_state="auto", layout="wide"
 )
 
-# st.title("WARP Scanner")
+#using html to set the title
 st.markdown(
     "<h1 style='text-align: center; color: Black;font-size: 50px'>WARP Scanner</h1>",
     unsafe_allow_html=True,
 )
 st.subheader("Input IP address or Domain ")
+
+#getting domain or ip from user in st text input
 scan_ip = st.text_input("example: 8.8.8.8 or scanme.nmap.org", "scanme.nmap.org")
 
 # get port range from user in st number input
@@ -28,6 +32,9 @@ port_range_end = st.number_input(
 )
 
 
+
+#convert domain to ip
+
 def ConvertToIP(scan_ip):
     try:
         ip = socket.gethostbyname(scan_ip)
@@ -37,6 +44,7 @@ def ConvertToIP(scan_ip):
         return "Invalid Domain"
 
 #Scan for OS detection
+
 def PortScannerOS(scanned_ip, portsopen):
     scanner = nmap.PortScanner()
     scanner.scan(scanned_ip, arguments="-O -T4 -p" + portsopen)
@@ -54,7 +62,10 @@ def PortScannerOS(scanned_ip, portsopen):
     df = pd.read_csv(StringIO(scanner.csv()), index_col=0, sep=";")
     st.dataframe(df, use_container_width=True)
     st.write(scanner.csv())
+    st.code(scanner.scaninfo(), language="json")
 
+
+#Scan for service version detection
 
 def PortScannerSV(scanned_ip, portsopen):
     scanner = nmap.PortScanner()
@@ -73,7 +84,9 @@ def PortScannerSV(scanned_ip, portsopen):
     df = pd.read_csv(StringIO(scanner.csv()), index_col=0, sep=";")
     st.dataframe(df, use_container_width=True)
     st.write(scanner.csv())
+    st.code(scanner.scaninfo(), language="json")
 
+#scan for open ports
 
 def getopenports(scanned_ip):
     opports = ""
@@ -90,6 +103,7 @@ def getopenports(scanned_ip):
         soc.close()
     return opports
 
+#implementing os detection
 
 if st.button("OS Scan"):
     scanned_ip = ConvertToIP(scan_ip)
@@ -99,6 +113,8 @@ if st.button("OS Scan"):
     st.write(portsopen)
     PortScannerOS(scanned_ip, portsopen)
 
+#SV scan button
+
 if st.button("SV Scan"):
     scanned_ip = ConvertToIP(scan_ip)
     portsopen = getopenports(scanned_ip)
@@ -106,6 +122,90 @@ if st.button("SV Scan"):
     print(str(portsopen))
     st.write(portsopen)
     PortScannerSV(scanned_ip, portsopen)
+
+
+
+#implementing traceroute
+if st.button ("traceroute scan"):
+    scanned_ip = ConvertToIP(scan_ip)
+    portsopen = getopenports(scanned_ip)
+    st.subheader("Open Ports")
+    print(str(portsopen))
+    st.write(portsopen)
+    scanner = nmap.PortScanner()
+    scanner.scan(scanned_ip, arguments="-sn -traceroute -T4 -p" + portsopen)
+    for i in scanner.all_hosts():
+        if scanner[i].state() == "up":
+            st.write(f"Host: {i} ({scanner[i].hostname()})")
+            st.write(f"State: {scanner[i].state()}")
+            for protocols in scanner[i].all_protocols():
+                st.write(f"Protocol: {protocols}")
+                lport = scanner[i][protocols].keys()
+                for port in lport:
+                    st.write(
+                        f"Port: {port} \t State: {scanner[i][protocols][port]['state']}"
+                    )
+    df = pd.read_csv(StringIO(scanner.csv()), index_col=0, sep=";")
+    st.dataframe(df, use_container_width=True)
+    st.write(scanner.csv())
+    st.code(scanner.scaninfo(), language="json")
+
+
+#implementing brute force scan
+if st.button ("Brute Force Scan"):
+    scanned_ip = ConvertToIP(scan_ip)
+    portsopen = getopenports(scanned_ip)
+    st.subheader("Open Ports")
+    print(str(portsopen))
+    st.write(portsopen)
+    scanner = nmap.PortScanner()
+    scanner.scan(scanned_ip, arguments="--script brute -T4 -p" + portsopen)
+    for i in scanner.all_hosts():
+        if scanner[i].state() == "up":
+            st.write(f"Host: {i} ({scanner[i].hostname()})")
+            st.write(f"State: {scanner[i].state()}")
+            for protocols in scanner[i].all_protocols():
+                st.write(f"Protocol: {protocols}")
+                lport = scanner[i][protocols].keys()
+                for port in lport:
+                    st.write(
+                        f"Port: {port} \t State: {scanner[i][protocols][port]['state']}"
+                    )
+    df = pd.read_csv(StringIO(scanner.csv()), index_col=0, sep=";")
+    st.dataframe(df, use_container_width=True)
+    st.write(scanner.csv())
+    st.code(scanner.scaninfo(), language="json")
+
+#implementing dos scan
+if st.button ("DOS Scan"):
+    scanned_ip = ConvertToIP(scan_ip)
+    portsopen = getopenports(scanned_ip)
+    st.subheader("Open Ports")
+    print(str(portsopen))
+    st.write(portsopen)
+    scanner = nmap.PortScanner()
+    scanner.scan(scanned_ip, arguments="--script dos -T4 -p" + portsopen)
+    for i in scanner.all_hosts():
+        if scanner[i].state() == "up":
+            st.write(f"Host: {i} ({scanner[i].hostname()})")
+            st.write(f"State: {scanner[i].state()}")
+            for protocols in scanner[i].all_protocols():
+                st.write(f"Protocol: {protocols}")
+                lport = scanner[i][protocols].keys()
+                for port in lport:
+                    st.write(
+                        f"Port: {port} \t State: {scanner[i][protocols][port]['state']}"
+                    )
+    df = pd.read_csv(StringIO(scanner.csv()), index_col=0, sep=";")
+    st.dataframe(df, use_container_width=True)
+    st.write(scanner.csv())
+    st.code(scanner.scaninfo(), language="json")
+
+
+
+
+
+#NSE script selection
 
 script = st.selectbox(
     "select your NSE script",
@@ -126,6 +226,9 @@ script = st.selectbox(
         "vuln",
     ],
 )
+
+
+#implementing nse scan
 
 if st.button("NSE Scan"):
     scanned_ip = ConvertToIP(scan_ip)
@@ -152,6 +255,10 @@ if st.button("NSE Scan"):
     st.code(scanner.scaninfo(), language="json")
 
 
+
+
+
+#hide footer
 st.markdown(
     """
     <style>
@@ -162,3 +269,13 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
+
+
+#hide hamburger menu
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
